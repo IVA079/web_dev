9a0bfb51bd2116ac86c92524c7291fe3363b110b
IVA079 <anastasiasaat81@gmail.com>
2025-10-11 06:01:15 +0600

    arrow function with this keyword

--- jsBasics.js
+++ jsBasics.js
@@ -99,3 +99,57 @@ console.log(" befor time out ");
 setTimeout(() => {
     console.log(" set time out  is 4 s");
 }, 4000);// you will see "set time out  is 4 s" this line after 4 s
+
+
+
+
+
+
+
+
+///////////arrow  function with  this kwyword
+
+const student=
+{
+    name:"newvy",
+    marks:90,
+    prop:this,
+    getName:function()
+    {
+        console.log(this);
+        console.log(this.name);   //this  keyword point to student object
+                                  //what is the scope of student object?global
+                                  //for normal function this  keyword means object scope
+        
+    },
+
+    getnameByArrowFunc:()=>
+    {
+     console.log(this);            
+        console.log(this.name);///rule of this " this keyword" for arrow function: parents scope
+        //who is the parent of "getnameByArrowFunc:()" function?student
+        //what is the scope of student object?global
+        //for arrow function this means parent scope
+
+    },
+    funcname3:function()
+    {
+setTimeout(()=>
+{
+    console.log("set time out function inside fun3 ");
+    console.log(this);///.   parent of set time out function  is func3
+    //the scope of func 3 is student 
+},3000
+);
+    },
+
+ funcname4:function()
+    {
+
+        setTimeout(function()
+    {
+        console.log(this);//this global object .as settimeout function inside window object
+    },2000);
+
+}
+};
