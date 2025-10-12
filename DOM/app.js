9dda6a14af50abc537c07648099d74dca09e0941
IVA079 <anastasiasaat81@gmail.com>
2025-10-13 02:32:04 +0600

    dom problem

--- DOM/app.js
+++ DOM/app.js
@@ -1,1 +1,22 @@
+
+//Document Object Model — how the browser understands and manipulates your HTML
+
+
+
 document.getElementById(mainImg);
+
+
+let copy=document.getElementById("mainImg");
+console.dir(document);
+console.log(copy);
+console.dir(copy);
+
+
+console.log(copy.src);
+console.log(copy.tagName);
+console.log(copy.id);
+//dir for object form
+
+
+
+console.log(document.firstChild);
