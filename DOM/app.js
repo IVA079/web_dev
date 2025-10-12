460a7c4133c1b58030a9884a83bd68d49e60d337
IVA079 <anastasiasaat81@gmail.com>
2025-10-13 03:37:47 +0600

    selecting element

--- DOM/app.js
+++ DOM/app.js
@@ -20,3 +20,44 @@ console.log(copy.id);
 
 
 console.log(document.firstChild);
+console.log(document.firstElementChild);
+
+
+console.log(document.firstElementChild.firstElementChild);  // <head>
+console.log(document.firstElementChild.lastElementChild);   // <body>
+/**
+  <!DOCTYPE html>
+<html>
+  <head>
+    <title>Example</title>
+  </head>
+  <body>
+    <h1>Hello DOM!</h1>
+  </body>
+</html>
+
+ 
+ 
+ *
+  Document
+ ├── #doctype (c)       ← special node for <!DOCTYPE html>
+ └── html(ec)
+      ├── head
+      │    └── title
+      └── body
+           └── h1
+
+ */
+
+
+
+           //document.firstChild: It gives you the first node inside the document.
+//Nodes include everything: tags, text, comments, or even the DOCTYPE.
+
+
+
+//document.firstElementChild: It gives you the first real HTML tag, skipping things like #doctype or text nodes.
+
+
+
+
