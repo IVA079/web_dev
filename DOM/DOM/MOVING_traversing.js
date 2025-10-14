bd12527c1f7be1644bc5c8c327979e0b31c46a0b
IVA079 <anastasiasaat81@gmail.com>
2025-10-14 11:32:24 +0600

    navigation

--- DOM/DOM/MOVING_traversing.js
+++ DOM/DOM/MOVING_traversing.js
@@ -1,7 +1,7 @@
 
 
 ///////parent and child: ///a parent can have many child .but a child  have one parent
-
+console.log("navigation");
 document.querySelector('b');
 
 
@@ -20,4 +20,36 @@ e6.children;
 e6.children[1].parentElement;
 
 
-///
+//////////////sibling////////////////////////////
+
+
+/**
+ * <p> (parent)
+ ├── <b>Spider-Man</b>   ← boldTag
+ │       ↓ nextElementSibling
+ ├── <a>Marvel Comics</a> ← firstLink
+ │       ↑ previousElementSibling
+ │       ↓ nextElementSibling
+ └── <a>Amazing Fantasy</a>
+
+ */
+
+let bold3=document.querySelectorAll('b')[3];
+
+let firstLink = document.querySelectorAll("a")[7];
+
+let childrenall=boldTag.children;
+
+let p2=document.querySelectorAll('p')[2];
+
+console.log(bold3.nextElementSibling);     
+//  <a href="#">Marvel Comics</a>
+
+console.log(firstLink.previousElementSibling);
+//  <b>Spider-Man</b>
+
+console.log(firstLink.nextElementSibling);
+//  <a href="#">Amazing Fantasy</a>
+
+console.log(boldTag.previousElementSibling);
+//  null (no element before <b>)
