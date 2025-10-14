18e83907ae42554dedb08d04cce41201913ae26f
IVA079 <anastasiasaat81@gmail.com>
2025-10-14 08:34:26 +0600

    class add

---
+++ DOM/DOM/classList.js
@@ -0,0 +1,16 @@
+let heading5=document.createElement('h1');
+console.log(heading5);
+
+console.log(heading5.classList);
+
+
+heading5.classList.add('color_name_wise_class');
+
+heading5.classList
+
+heading5.classList.add('class 2');
+
+heading5.classList.add('class 3');
+
+heading5.classList;
+
