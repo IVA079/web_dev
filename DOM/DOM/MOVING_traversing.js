0460d1afa980b07735af7c636baada01dd5fecf3
IVA079 <anastasiasaat81@gmail.com>
2025-10-14 09:31:45 +0600

    Update MOVING_traversing.js

--- DOM/DOM/MOVING_traversing.js
+++ DOM/DOM/MOVING_traversing.js
@@ -1,1 +1,23 @@
+
+
+///////parent and child: ///a parent can have many child .but a child  have one parent
+
 document.querySelector('b');
+
+
+
+
+
+let e6=document.querySelector('b').parentElement;
+
+e7=e6.parentElement;
+
+
+
+e6.childElementCount;
+e6.children;
+
+e6.children[1].parentElement;
+
+
+///
