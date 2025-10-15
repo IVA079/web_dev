dc6c37a0439d0c3ea2e52929c43863104a32d063
IVA079 <anastasiasaat81@gmail.com>
2025-10-15 08:47:46 +0600

    on click

--- flower_html_js/flower.js
+++ flower_html_js/flower.js
@@ -68,3 +68,52 @@ thumbnails[1].onclick = function() {
 
 **///when you click on thumbnails[0],function will change main image to thumbnails[0] image .same for all
 //for previous ex1  if we use loop will it create overwrite problem and last i =7 so maing image will not be converted to gallery image
+
+
+
+
+
+
+
+
+
+//click on button
+let a=document.querySelectorAll('p');
+
+
+let p_out=document.querySelectorAll('p')[4];
+
+let btn=document.querySelectorAll('button')
+
+
+let btn4=document.querySelectorAll('button')[4];
+
+
+function click_on_button()
+{
+    p_out.textContent="🌸 ";
+
+}
+
+//btn4.onclick=click_on_button();
+
+
+
+
+
+
+
+let nameInput = document.getElementById('name');
+let flowerType = document.getElementById('flowerType');
+let commentBox = document.getElementById('comment');
+let submitBtn = document.getElementById('submitBtn');
+let output = document.getElementById('output'); //
+
+submitBtn.onclick = function() {
+  if (nameInput.value && flowerType.value) {
+    output.textContent = `Hello ${nameInput.value}! You love ${flowerType.value} because "${commentBox.value}". 🌸`;
+  } else {
+    output.textContent = "⚠️ Please fill in your name and choose a flower.";
+  }
+  commentBox.value = ""; // clear comment
+};
