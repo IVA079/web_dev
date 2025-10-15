c4a577a969a4b705f5c2afc273928c581dc9b7e1
IVA079 <anastasiasaat81@gmail.com>
2025-10-15 07:48:27 +0600

    Update flower.js

--- flower_html_js/flower.js
+++ flower_html_js/flower.js
@@ -13,10 +13,17 @@ let mainimg=document.getElementById('mainImage');
 
 
 let index = 0;
+////number of  function calling = how many time you click on main images
 mainimg.onclick = function() {
   index++;
-  if (index >= imgs.length) index = 0;
+  if (index >= imgs.length){
+    index = 0;
+}
+else
+{
   mainimg.src = imgs[index].src;
+}
+
 };
 
 
@@ -24,4 +31,40 @@ mainimg.onclick = function() {
 
 
 
+/////example 2
+let mainImage = document.getElementById('mainImage');
+let thumbnails = document.querySelectorAll('.gallery img');
 
+
+for (let i = 0; i < thumbnails.length; i++) {
+  thumbnails[i].onclick = function() {
+    
+    mainImage.src = thumbnails[i].src;
+    mainImage.alt = thumbnails[i].alt;
+
+ 
+    for (let j = 0; j < thumbnails.length; j++) {
+      thumbnails[j].style.border = "2px solid #ccc";
+    }
+    thumbnails[i].style.border = "3px solid hotpink";
+  };
+}
+
+
+
+///explanation
+/** 
+thumbnails[0].onclick = function() {
+    
+    mainImage.src = thumbnails[0].src;
+    mainImage.alt = thumbnails[0].alt;
+}
+
+thumbnails[1].onclick = function() {
+    
+    mainImage.src = thumbnails[1].src;
+    mainImage.alt = thumbnails[1].alt;
+}
+
+**///when you click on thumbnails[0],function will change main image to thumbnails[0] image .same for all
+//for previous ex1  if we use loop will it create overwrite problem and last i =7 so maing image will not be converted to gallery image
