fb1050868f896e773a29a072d78f98854c5db10b
IVA079 <anastasiasaat81@gmail.com>
2025-10-27 10:36:45 +0600

    .

--- Backend/expressDir/index3.js
+++ Backend/expressDir/index3.js
@@ -1,6 +1,11 @@
 let express=require("express");
 
 console.log(express());
-
+console.log("......................next line ");
 
 console.log(express);
+
+
+
+console.log("line 10");
+console.log(typeof express);
