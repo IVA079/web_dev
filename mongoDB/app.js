a49eb93ce4d163eef349c8b5ef603218af046699
IVA079 <anastasiasaat81@gmail.com>
2025-11-01 08:33:50 +0600

    Update app.js

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -1,4 +1,29 @@
-const mongoose = require('mongoose');
+          
+/**
+ * What You Were Doing Before
+
+You were using MongoDB shell — typing commands like:
+
+db.dogs.insertOne(...)
+db.cats.find(...)
+db.movies.deleteOne(...)
+
+
+That means you were talking directly to the database, just to learn basic Mongo commands.
+
+But in real life, apps don’t do that manually.
+
+
+
+
+Mongoose helps Node.js apps connect to MongoDB easily and work with data using schemas, validation, and JavaScript objects — making your life easier.
+
+
+ */
+
+          
+          
+          const mongoose = require('mongoose');
 
 
 
@@ -22,3 +47,11 @@ console.log("err"+err);
 
 
 main();
+
+
+
+
+
+
+
+//////////////schema//////////////////////////////
