16b39937f9ff2b94f63a5c83112868981282d54b
IVA079 <anastasiasaat81@gmail.com>
2025-11-03 15:06:17 +0600

    component

--- React/basic-react-app/src/App.jsx
+++ React/basic-react-app/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Greeting from './Greeting';
 
 function App() {
-  
-return <h1>
-  hello world
-</h1>
+  return (
+    <div>
+      <h1>Component 1</h1>
+    
+<Greeting/>
 
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ React/basic-react-app/src/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting()
+{
+    return <h2>hello, welcome to my website</h2>
+}
+
+
+export default Greeting;
+
+
